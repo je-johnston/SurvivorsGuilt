be49c4dfccb0c79877cac9c58b33e51030d5dab3
James Johnston <je_johnston@outlook.com>
2018-02-27 15:55:17 -0800

    Added music as well as toggle button to start/stop music.

--- SurvivorsGuilt/audPlayer.js
+++ SurvivorsGuilt/audPlayer.js
@@ -5,10 +5,35 @@ class audPlayer {
 
         console.log("Audio beginning");
 
-        var music = new Audio('./Assets/Audio/scavengers_music.mp3');
-        music.play();
-        music.loop = true;
+        this.music = new Audio('./Assets/Audio/scavengers_music.mp3');
+        this.music.play();
+        this.music.loop = true;
 
+        this.audButton = document.getElementById("musicToggler");
+
+        var that = this;
+        this.audButton.onclick = function () {
+            that.toggleMusic();
+        }
+
+        this.audButton.onmousedown = function () {
+            that.audButton.style.fontWeight = "bold";
+        }
+
+        this.audButton.onmouseup = function () {
+            that.audButton.style.fontWeight = "normal";
+        }
+
+
+
+    }
+
+    toggleMusic() {
+        if (this.music.paused) {
+            this.music.play();
+        } else {
+            this.music.pause();
+        }
     }
 
 
